2d1246c821a2c2a39ec79e965d2d31131d035874
jiaokang <Administrator@HP201-1>
2017-06-13 15:09:55 +0800

    连接数据库

---
+++ Demo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 连接字符串
+
+            #endregion
+            //获取连接字符串方式
+            /*1.Data Soucre:服务器名称
+             * Initial Catalog:数据库名称
+             *Integrated Security=SSPI:采用windows身份认证模式(集成安全)
+             2.SQL server身份验证
+             需要指定用户名和密码
+
+             */
+            string strConn = "Data Source=HP201-1;Initial Catalog=Student;Integrated Security=SSPI;";
+
+            string strConn1 = "Data Source=(localhost);Inital Catalog=Student;User ID=sa;Password=''123";
+            
+            //创建连接对象
+            SqlConnection con = new SqlConnection(strConn);
+            try
+            {
+            con.Open();
+            Console.WriteLine("打开连接");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
