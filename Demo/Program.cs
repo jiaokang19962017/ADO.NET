aa0656f99e87db6e4c54d8180db5c68c74e76783
jiaokang <Administrator@HP201-1>
2017-06-13 16:26:43 +0800

    获取连接字符串两种方式

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -11,24 +11,23 @@ namespace Demo
     class Program
     {
 
-        #region 应用程序配置
-      
-
-
-        #endregion
+   
       
         static void Main(string[] args)
         {
-            /*
+            #region 应用程序配置
+      /*
        从app.config(应用配置文件)中读取连接字符串
        1.需要导入System.Configruation.dll文件
        2.引入System.Configruation命名空间
        3.使用ConfigruationManager.ConnectionString["strConn"].ToString()来读取字符串
            */
-            string strConn = ConfigurationManager.ConnectionStrings["strConn"].ToString();
-            #region 连接字符串
+
 
             #endregion
+            //从配置文件app.config中获取连接字符串
+            string strConn = ConfigurationManager.ConnectionStrings["strConn"].ToString();
+            #region 连接字符串
             //获取连接字符串方式
             /*1.Data Soucre:服务器名称
              * Initial Catalog:数据库名称
@@ -36,12 +35,15 @@ namespace Demo
              2.SQL server身份验证
              需要指定用户名和密码
              */
-            /*string strConn = "Data Source=HP201-1;Initial Catalog=Student;Integrated Security=SSPI;";
 
-             string strConn1 = "Data Source=(localhost);Inital Catalog=Student;User ID=sa;Password=''123";
-              */
             //创建连接对象
             //打开指定的数据源
+            #endregion
+            //手动书写连接字符串
+       
+            //string strConn = "Data Source=HP201-1;Initial Catalog=Student;Integrated Security=SSPI;";----------------windows认证连接方式
+            //string strConn1 = "Data Source=(localhost);Inital Catalog=Student;User ID=sa;Password=''123";--------------sql身份认证方式
+            
             SqlConnection con = new SqlConnection(strConn);
             try
             {
